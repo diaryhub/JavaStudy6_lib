59a056f0533ab5940bb40a698a905f7d172adbf6
diaryhub <kjw991953@gmail.com>
2022-01-10 17:00:34 +0900

    indexOf

--- src/com/syudy/s1/lang/string/StringStudy1.java
+++ src/com/syudy/s1/lang/string/StringStudy1.java
@@ -7,7 +7,7 @@ public class StringStudy1 {
 		String name2 = new String("hello");
 		System.out.println(name);
 		System.out.println(name2);
-		
+		//charAt(i) == 문자열의 i번째 문자를 찾아서 문자 호출 
 		char ch = name.charAt(1);
 		
 		System.out.println(ch);
@@ -31,6 +31,35 @@ public class StringStudy1 {
 		System.out.println(str3.equals(str4));
 		
 		
+	}
+	
+	public void indexTest() {
+		
+		String msg = "Hello World!";
+		//indexOf == 문자 또는 문자열이 몇번째 글자부터 시작인지 찾아서 정수 호출
+		int num = msg.indexOf('W');
+		System.out.println(num);
+	}
+	
+
+	public void lengthTest() {
+		String title = "West Side Story";
+		int len = title.length();
+		System.out.println(len);
+		for(int i=0;i<len;i++) {
+			System.out.println(title.charAt(i));
+		}
+		
+		String find = "S";
+		int count=0;
+		for(int i=0;i<title.length();i++) {
+			int index = title.indexOf(find,i);
+			if(index>=0) {
+				count ++;
+				i=index;
+			}
+		}
+		System.out.println(count);
 	}
 
 }
