d17f60d9251e25bb0b078286668b4d0470f6b310
diaryhub <kjw991953@gmail.com>
2022-01-11 17:25:27 +0900

    StringBuffer

---
+++ src/com/syudy/s1/lang/stringBuffer/StringBufferMain.java
@@ -0,0 +1,20 @@
+package com.syudy.s1.lang.stringBuffer;
+
+public class StringBufferMain {
+
+	public static void main(String[] args) {
+		StringBuffer sb = new StringBuffer();
+		
+		sb.append("diary");
+		sb.append("일기");
+		System.out.println(sb);
+		String name = "diary";
+		String name2 = "일기";
+		name = name + name2;
+		
+		boolean check = name.equals(sb.toString());
+		
+		System.out.println(check);
+	}
+
+}
